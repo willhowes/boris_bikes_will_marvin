453acd6026de0643acaba9d3faa0a2f53b539e42
hjdr <harry.jdr@gmail.com>
2019-06-26 16:31:20 +0100

    created the receive_bike meathod which stores bikes received into an array (stage 11)

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,7 +1,20 @@
 DockingStation = Class.new
 
 class DockingStation
+
+  attr_reader :available_bikes
+
+  def initialize
+    @available_bikes = []
+  end
+
   def release_bike
     Bike.new
   end
+
+  def receive_bike(bike)
+    @available_bikes << bike
+    @available_bikes
+  end
+
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -9,8 +9,30 @@ describe DockingStation do
     it 'creates a Bike class when method is called' do
       expect(subject.release_bike).to be_an_instance_of(Bike)
     end
+    
     it 'releases working bikes' do
       expect(subject.release_bike).to be_working
     end
   end
+
+  describe "#receive_bike" do
+    it "responds to receive_bike" do
+      expect(subject).to respond_to(:receive_bike)
+    end
+  end
+
+  it "stores the user's bike" do
+    boris_bike = Bike.new
+    expect(subject.receive_bike(boris_bike)).to include(boris_bike)
+  end
+
+  describe "@available_bikes" do
+    it "lists all of the available bikes at the station" do
+      station = DockingStation.new
+      boris_bike = Bike.new
+      station.receive_bike(boris_bike)
+      expect(station.available_bikes).to eq([boris_bike])
+    end
+  end
+
 end
